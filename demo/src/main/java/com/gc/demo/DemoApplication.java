07e743d8c8a13d22c4639c73a1de954da14ade78
chengao <chengao@Ctrip.com>
2018-08-13 11:14:21 +0800

    fix

--- demo/src/main/java/com/gc/demo/DemoApplication.java
+++ demo/src/main/java/com/gc/demo/DemoApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = { "classpath:application-bean.xml" })
+@ImportResource(locations = {"classpath:application-bean.xml"})
 public class DemoApplication {
 
-    public static void main(String[] args) {
-	SpringApplication.run(DemoApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
 }
